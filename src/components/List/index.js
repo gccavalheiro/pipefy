9fc0041ff7641a2f6a19661a4ffee9c63942bf11
Gabriel Cavalheiro <gccavalheiro94@gmail.com>
2020-03-13 20:56:30 -0300

    Versão 1

---
+++ src/components/List/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { MdAdd } from 'react-icons/md';
+
+import Card from '../Card';
+
+import { Container } from './styles';
+
+const List = ({ data, index: listIndex }) => {
+    return (
+        <Container done={data.done}>
+            <header>
+                <h2>{data.title}</h2>
+                {data.creatable && (
+                    <button
+                        type="button"
+                    >
+                        <MdAdd
+                            size={24}
+                            color="#fff"
+                        />
+                    </button>
+                )}
+            </header>
+
+            <ul>
+                {data.cards.map((card, index) => {
+                    return (
+                        <Card
+                            key={card.id}
+                            listIndex={listIndex}
+                            index={index}
+                            data={card}
+                        />
+                    )
+                })}
+            </ul>
+        </Container>
+    );
+}
+
+export default List;
